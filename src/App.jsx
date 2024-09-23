b393b4a7f93d928365308297b079f21c2cad8970
foad <foadhosseini32@gmail.com>
2024-09-23 16:33:25 +0330

    split bill

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ const initialRes = [
 function App() {
   const [resturantsInfo, setResturantsInfo] = useState(initialRes);
   const [selectRes, setSelectRes] = useState(null);
+  const [showAddFriend, setShowAddFriend] = useState(false);
 
   function SelectedRes(SelectedRes) {
     setSelectRes(prevRes =>
@@ -34,15 +35,24 @@ function App() {
 
   function addresturant(res) {
     setResturantsInfo(prev => [...prev, res])
+    setShowAddFriend(false)
+  }
+
+  function handleSplitBill(value) {
+    setResturantsInfo(prev => prev.map(res => res.id === selectRes.id ? { ...res, balance: res.balance + value } : res))
+    setSelectRes(null)
   }
 
   return (
     <div className='app'>
       <div className='sidebar'>
         <Resturants onResInfo={resturantsInfo} onSelect={SelectedRes} selectRes={selectRes} />
-        <FormAddResturant onAddRes={addresturant} />
+        {showAddFriend && <FormAddResturant onAddRes={addresturant} />}
+        <div style={{ marginTop: "10px" }}>
+          <Button clickIt={() => setShowAddFriend(show => !show)}>{showAddFriend ? "Close" : "Add resturant"}</Button>
+        </div>
       </div>
-      {selectRes && <SplitBillFrom />}
+      {selectRes && <SplitBillFrom info={selectRes} onSplit={handleSplitBill} />}
     </div>
   )
 }
@@ -67,7 +77,7 @@ function ResturantInfo({ info, onSelect, selectRes }) {
       <h4>{info.resName}</h4>
 
       {info.balance == 0 && <p>{`${info.resName} is even with you`}</p>}
-      {info.balance > 0 && <p className='green'>{`${info.resName} ows you ${Math.abs(info.balance)})`}</p>}
+      {info.balance > 0 && <p className='green'>{`${info.resName} ows you ${Math.abs(info.balance)}`}</p>}
       {info.balance < 0 && <p className='red'>{`You owe ${info.resName} ${Math.abs(info.balance)}`}</p>}
 
 
@@ -111,27 +121,35 @@ function FormAddResturant({ onAddRes }) {
   )
 }
 
-function SplitBillFrom() {
-  function handleSubmit() {
+function SplitBillFrom({ info, onSplit }) {
+  const [paidByUser, setPaidByUser] = useState("");
+  const [bill, setBill] = useState();
+  const friendPaid = bill ? bill - paidByUser : "";
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSplit(whoIsPaying === "user" ? friendPaid : -paidByUser)
 
   }
   return (
     <form className='form-split-bill' onSubmit={handleSubmit}>
-      <h2>Split title with X</h2>
+      <h2>Split title with {info.resName}</h2>
 
       <label>Bill value</label>
-      <input type="text" />
+      <input type="text" value={bill} onChange={e => setBill(Number(e.target.value))} />
 
       <label>Your expense</label>
-      <input type="text" />
+      <input type="text" value={paidByUser} onChange={e => setPaidByUser(Number(e.target.value > bill ? paidByUser : e.target.value))} />
 
-      <label>X's expense</label>
-      <input type="text" />
+      <label>{info.resName}'s expense</label>
+      <input type="text" disabled value={friendPaid} />
 
       <label>Who is paying the bill</label>
-      <select>
+      <select onChange={e => setWhoIsPaying(e.target.value)}>
         <option value="user">You</option>
-        <option value="resturant">X</option>
+        <option value="resturant">{info.resName}</option>
       </select>
 
 
